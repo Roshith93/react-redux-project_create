963cc07722e96f89ca01a6eac9e2fa2197bce092
Roshith R <r.roshith@gmail.com>
2021-02-13 21:38:52 +0530

    Intial folder structure of the app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <div className='App'>Hello there</div>
+    </Router>
+  )
+}
+
+export default App
