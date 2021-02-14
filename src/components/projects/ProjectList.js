4f63ff9d81082d52ef0f5c4d258affb99a695bf6
Roshith R <r.roshith@gmail.com>
2021-02-14 08:51:28 +0530

    Project summary added to project list

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,27 +1,10 @@
+import {ProjectSummary} from './ProjectSummary'
 export const ProjectList = () => {
   return (
     <div className='project-list section'>
-      <div className='card z-depth-0 project-summary'>
-        <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-          <p>Posted by Net Ninja</p>
-          <p className='grey-text'>Today 2 am</p>
-        </div>
-      </div>
-      <div className='card z-depth-0 project-summary'>
-        <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-          <p>Posted by Net Ninja</p>
-          <p className='grey-text'>Today 2 am</p>
-        </div>
-      </div>
-      <div className='card z-depth-0 project-summary'>
-        <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-          <p>Posted by Net Ninja</p>
-          <p className='grey-text'>Today 2 am</p>
-        </div>
-      </div>
+     <ProjectSummary/>
+     <ProjectSummary/>
+     <ProjectSummary/>
     </div>
   )
 }
